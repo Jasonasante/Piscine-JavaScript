5195297e1845141b29718a8a8b8f36926433088a
jasonasante <jason.asante@outlook.com>
2022-06-06 16:13:19 +0100

    learn how to round numbers to 2 decimal places

--- dog-years.js
+++ dog-years.js
@@ -1,22 +1,18 @@
 function dogYears(planetName,dogAge){
-    const sec=31557600
-if (planetName==='earth'){
-    return Number(((dogAge*7)/sec).toFixed(2));
-}else if (planetName==='mercury'){
-    return Number(((dogAge*0.2408467*7)/sec).toFixed(2));
-}else if (planetName==='venus'){
-    return (Number((dogAge*0.61519726*7)/sec).toFixed(2));
-}else if (planetName==='mars'){
-    return Number(((dogAge*1.8808158*7)/sec).toFixed(2));
-}else if (planetName==='jupiter'){
-    return Number(((dogAge*11.862615*7)/sec).toFixed(2));
-}else if (planetName=='saturn'){
-    return Number(((dogAge*29.447498*7)/sec).toFixed(2));
-}else if (planetName==='uranus'){
-    return Number(((dogAge*84.016846*7)/sec).toFixed(2));
-}else if (planetName==='neptune'){
-    return Number(((dogAge*164.79132*7)/sec).toFixed(2));
-}else{
-    return null
+    const Planets={
+        'earth':1,
+        'mercury':0.2408467,
+        'venus':0.61519726,
+        'mars':1.8808158,
+        'jupiter':11.862615,
+        'saturn':29.447498,
+        'uranus':84.016846,
+        'neptune':164.79132,
+    };
+    const sec=31557600;
+let year=dogAge/sec;
+let newYear=year/Planets[planetName];
+let newDogYear=newYear*7;
+return Number(newDogYear.toFixed(2));
 }
-}
+console.log(dogYears('mercury', 2134835688))
