964b89cc4cb869b038cd6526ecccc8b8867398f2
jasonasante <jason.asante@outlook.com>
2022-06-06 17:43:43 +0100

    Create a function that takes one number argument; returning 1 if the number is positive, -1 if the number is negative and 0 if the number is exactly 0.Also create function that confirms whether two arguments have the same sign

--- sign.js
+++ sign.js
@@ -1,9 +1,17 @@
 function sign(num){
     if (num===0){
         return 0;
-    }else if (num>0){
+    }else if (num>=0){
         return 1;
     }else{
         return -1;
+    }
+}
+
+function sameSign(num1, num2){
+    if (sign(num1)===sign(num2)){
+        return true
+    }else{
+        return false
     }
 }
