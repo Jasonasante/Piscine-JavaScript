306cfd571d0349662b7e00acfc2a45d814e3352f
jasonasante <jason.asante@outlook.com>
2022-06-06 15:34:52 +0100

    learn how to round numbers to 2 decimal places

--- dog-years.js
+++ dog-years.js
@@ -3,19 +3,19 @@ function dogYears(planetName,dogAge){
 if (planetName==='earth'){
     return (dogAge/sec).toFixed(2);
 }else if (planetName==='mercury'){
-    return ((dogAge*0.2408467)/sec).toFixed(2);
+    return ((dogAge*0.2408467*7)/sec).toFixed(2);
 }else if (planetName==='venus'){
-    return ((dogAge*0.61519726)/sec).toFixed(2);
+    return ((dogAge*0.61519726*7)/sec).toFixed(2);
 }else if (planetName==='mars'){
-    return ((dogAge*1.8808158)/sec).toFixed(2);
+    return ((dogAge*1.8808158*7)/sec).toFixed(2);
 }else if (planetName==='jupiter'){
-    return ((dogAge*11.862615)/sec).toFixed(2);
+    return ((dogAge*11.862615*7)/sec).toFixed(2);
 }else if (planetName=='saturn'){
-    return ((dogAge*29.447498)/sec).toFixed(2);
+    return ((dogAge*29.447498*7)/sec).toFixed(2);
 }else if (planetName==='uranus'){
-    return ((dogAge*84.016846)/sec).toFixed(2);
+    return ((dogAge*84.016846*7)/sec).toFixed(2);
 }else if (planetName==='neptune'){
-    return ((dogAge*164.79132)/sec).toFixed(2);
+    return ((dogAge*164.79132*7)/sec).toFixed(2);
 }else{
     return null
 }
