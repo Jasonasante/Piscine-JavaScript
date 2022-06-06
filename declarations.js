a07c280367f66b20408249113539c66bffff0d85
jasonasante <jason.asante@outlook.com>
2022-06-06 12:31:23 +0100

    learn to make constant variables

---
+++ declarations.js
@@ -0,0 +1,23 @@
+const escapeStr= "\`\\\/\"\'";
+Object.freeze(escapeStr);
+
+const arr= [4,'2'];
+Object.freeze(arr);
+
+const obj1= {
+ str :"My name is Jeff",
+ num :3,
+ bool :true,
+ undef :undefined
+} ;
+Object.freeze(obj1);
+
+const nested= {
+    arr1: [4, undefined, '2'],
+    obj:{
+        str : "My name is Joseph",
+        num : 2,
+        bool : false
+    }
+};
+Object.freeze(nested);
