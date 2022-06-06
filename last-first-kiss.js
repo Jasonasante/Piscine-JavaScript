254c75bf2b453a8c9648478e68ddd0541d01009b
jasonasante <jason.asante@outlook.com>
2022-06-06 13:38:56 +0100

    return the first, last and and both arguments

---
+++ last-first-kiss.js
@@ -0,0 +1,11 @@
+function first(array){
+return array[0];
+}
+
+function last (array){
+    return array[(array.length)-1];
+}
+
+function kiss (array){
+    return array[0], array[(array.length)-1];
+}
