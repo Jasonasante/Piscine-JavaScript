4532683c7599f18fa5cd505b7d486ac833ccca31
jasonasante <jason.asante@outlook.com>
2022-06-06 14:28:54 +0100

    concatenate two arguments

--- concat-str.js
+++ concat-str.js
@@ -1,8 +1,3 @@
 function concatStr(str1, str2){
-    if (typeof str1,str2 === 'string') {
-        let newStr=str1+str2;
-        return newStr
-    } else {
-        return null
-    }
+    return String(str1)+String(str2)
 }
