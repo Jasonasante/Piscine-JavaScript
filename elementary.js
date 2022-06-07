baa06f55c612a749be636e0768216aa4504af310
jasonasante <jason.asante@outlook.com>
2022-06-07 14:39:39 +0100

    create a multiply, divide and modulo function without their operators

---
+++ elementary.js
@@ -0,0 +1,49 @@
+function multiply(divided,divisor){
+    if (divisor==0){
+        return 0
+    }else if (divisor<0){
+        let num=-divided;
+        let result=0;
+        for (i=0;i>divisor;i--){
+            result+=num;
+        }
+        return result;
+    }else{
+        let num=divided;
+        let result=0;
+        for (i=0;i<divisor;i++){
+            result+=num;
+        }
+        return result;
+    }
+}
+
+function divide (divided,divisor){
+    if (divisor==0){
+        return null;
+    }else if (((divisor<0)&&(divided>0))||((divided<0)&&(divisor>0))){
+        let result =0;
+        divided=Math.abs(divided);
+        divisor=Math.abs(divisor);
+        while (divided >= divisor) {
+            divided -= divisor;
+            ++result;
+        }
+        return result*-1;
+    }else{
+        let result =0;
+        divided=Math.abs(divided);
+        divisor=Math.abs(divisor);
+        while (divided >= divisor) {
+            divided -= divisor;
+            ++result;
+        }
+        return result;
+    }
+}
+
+function modulo(num,divisor){
+    let b=divide(num,divisor);
+    let t =  num -multiply(b,divisor);
+    return t;
+}
