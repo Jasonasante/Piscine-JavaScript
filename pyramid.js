0a6e6d0af735521bfdeb75178870ff51cd8fde55
jasonasante <jason.asante@outlook.com>
2022-06-09 19:29:49 +0100

    the number of spaces before the string correspond with the length of the string

--- pyramid.js
+++ pyramid.js
@@ -2,11 +2,11 @@ function pyramid(strg, length) {
     let result = ""
     for (let i = 1; i <= length; i++) {
       if (i < length) {
-        let str = ' '.repeat(length - i);
+        let str = ' '.repeat(strg.length*(length - i));
         let str2 = strg.repeat(i * 2 - 1)
         result += str + str2+ '\n';
       } else {
-        let str = ' '.repeat(length - i);
+        let str = ' '.repeat(strg.length*(length - i));
         let str2 = strg.repeat(i * 2 - 1)
         result += str + str2;
       }
