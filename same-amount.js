5a969499954c0b636e1b2c872f52e820727af42e
jasonasante <jason.asante@outlook.com>
2022-06-10 18:20:36 +0100

    create a function that verifies whether there is the same number of regular expressions present a string

--- same-amount.js
+++ same-amount.js
@@ -1,5 +1,13 @@
 function sameAmount(str,reg1,reg2){
-    let one= new RegExp(reg1.source, reg1.flags + "g");
-    let two=new RegExp(reg2.source,reg2.flags + "g");
-    return ((str.match(one)).length===(str.match(two)).length);
+    let one=new RegExp(reg1,'g')
+    let two= new RegExp(reg2,'g')
+    let oneMatch=str.match(one);
+    let twoMatch=str.match(two);
+    if (oneMatch===null){
+    oneMatch=[];
+    }
+    if (twoMatch===null){
+    twoMatch=[]
+    }
+    return oneMatch.length===twoMatch.length;
 }
