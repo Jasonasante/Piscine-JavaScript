ea68ed62a41ebf2a0bb87c933e7e4e87117381fe
jasonasante <jason.asante@outlook.com>
2022-06-13 17:13:09 +0100

    date-is.js

---
+++ date-is.js
@@ -0,0 +1,20 @@
+function isValid(date) {
+    let d1=new Date(date)
+    return d1 instanceof Date && !isNaN(d1.valueOf());
+}
+  
+function isAfter(date1,date2){
+    return date1>date2
+}
+  
+function isBefore(date1,date2){
+    return date1<date2
+}
+
+function isFuture(date){
+    return isValid(date) && isAfter(date,Date.now())
+}
+
+function isPast(date){
+    return isValid(date) && isBefore(date,Date.now())
+}
