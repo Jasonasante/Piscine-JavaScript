59c054f15205be2c2957072ae4732fe37f2cb11c
jasonasante <jason.asante@outlook.com>
2022-06-14 17:25:33 +0100

    return the number of leap years from the given date

--- count-leap-years.js
+++ count-leap-years.js
@@ -1,5 +1,8 @@
 function countLeapYears(date){
     let year=date.getFullYear()
+    if (year<4){
+        return 0
+    }
     let numCenturies=year/100
     let num400=(numCenturies/4)-1
     let num=year/4
