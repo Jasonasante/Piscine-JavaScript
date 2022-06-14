88da1bfa8bbb76d75f537dbb5533f524797a1b74
jasonasante <jason.asante@outlook.com>
2022-06-14 17:03:55 +0100

    return true if date matches the conditions in the string

--- match-cron.js
+++ match-cron.js
@@ -1,29 +1,29 @@
 function matchCron(str,date){
-    let slice=str.split(' ')
-    if (slice.length!==5){
+    let arr=str.split(' ')
+    if (arr.length!==5){
         return false
     }
 
-    for (let i=0;i<slice.length;i++){
-        if (slice[i]!='*'){
+    for (let i=0;i<arr.length;i++){
+        if (arr[i]!='*'){
             if (i===0){
-                if (date.getMinutes()!=Math.round(slice[i])){
+                if (date.getMinutes()!=Math.round(arr[i])){
                     return false
                 }
             }else if (i===1){
-                if (date.getHours()!==Math.round(slice[i])){
+                if (date.getHours()!=Math.round(arr[i])){
                     return false
                 } 
             }else if (i===2){
-                if (date.getDate()!==Math.round(slice[i])){
+                if (date.getDate()!=Math.round(arr[i])){
                     return false
                 }
             }else if (i===3){
-                 if (date.getMonth()+1!==Math.round(slice[i])){
+                 if (date.getMonth()+1!=Math.round(arr[i])){
                     return false
                 }               
             }else{
-                if (date.getDay()!==Math.round((slice[i]))){
+                if (date.getDay()!=Math.round((arr[i]))){
                     return false
                 }
             }
