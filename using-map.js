161bb67e224d0e5a28800e6442d628863bb26f97
jasonasante <jason.asante@outlook.com>
2022-06-15 16:41:43 +0100

    map manipulation

--- using-map.js
+++ using-map.js
@@ -1,4 +1,21 @@
 function citiesOnly(array){
     let result = array.map(element => element.city)
     return result
-    }
+}
+
+const toUpper= str => str.replace(/(^\w|\s\w)(\S*)/g, (_,m1,m2) => m1.toUpperCase()+m2.toLowerCase()) 
+function upperCasingStates(arr) {
+    return arr.map(toUpper)
+}
+
+function fahrenheitToCelsius(arr){
+    return arr.map(str=>Math.floor((parseInt(str)-32)/1.8)+'°C')
+}
+
+function trimTemp(arr){
+    return arr.map(element => element.temperature = element.temperature.replace(/\s/g, ''))
+}
+
+function tempForecasts(arr){
+    return  arr.map(element => `${element.temperature = Math.floor((parseInt(element.temperature)-32)/1.8)+'°Celsius'} in ${element.city}, ${element.state=element.state.replace(/(^\w|\s\w)(\S*)/g, (_,m1,m2) => m1.toUpperCase()+m2.toLowerCase())}`)
+}
