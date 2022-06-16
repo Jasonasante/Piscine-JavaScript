91ac4cc2082fa28416c25c0a5f5eaea07b5b7e58
jasonasante <jason.asante@outlook.com>
2022-06-16 20:30:36 +0100

    filter manipulation without inbuilt

---
+++ filter.js
@@ -0,0 +1,27 @@
+function filter(arr = [], fun) {
+    let tempArr=[]
+    for (let i=0;i<arr.length;i++){
+        if (fun(arr[i])){
+            tempArr.push(arr[i])
+        }
+    }
+    return tempArr
+}
+
+function reject(arr = [], fun) {
+    let tempArr=[]
+    for (let i=0;i<arr.length;i++){
+        if (!fun(arr[i])){
+            tempArr.push(arr[i])
+        }
+    }
+    return tempArr
+}
+
+function partition(arr = [], fun) {
+    let tempArr=[]
+    let fit=filter(arr,fun)
+    let notFit=reject(arr,fun)
+    tempArr.push(fit,notFit)
+    return tempArr
+}
