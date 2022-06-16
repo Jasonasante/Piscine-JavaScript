de0b9ab0609dd3576c42e0f7162aa0f96d580de2
jasonasante <jason.asante@outlook.com>
2022-06-16 18:55:15 +0100

    create map and flat map function without method

---
+++ mapper.js
@@ -0,0 +1,31 @@
+function map(arr = [], fun) {
+    let tempArr=[]
+    for (let i = 0; i < arr.length; i++) {
+       tempArr.push(fun(arr[i], i, arr)) 
+    }
+    return tempArr
+}
+
+function flatMap (arr=[],fun){
+    let tempArr= flat(arr)
+    let tempArr1=[]
+    for (let i = 0; i < tempArr.length; i++) {
+        tempArr1.push(fun(tempArr[i], i, tempArr1)) 
+     }
+    return tempArr1
+}
+function flat(arr) {
+    let result = []; // In this we`ll save the output;
+    let main = arr,
+      first;
+    while (main.length > 0) { //1. Looping it     
+      first = main[0];
+      if (Array.isArray(first)) {
+        Array.prototype.splice.apply(main, [0, 1].concat(first));
+      } else {
+        result.push(first);
+        main.splice(0, 1);
+      }
+    }
+    return result;
+}
