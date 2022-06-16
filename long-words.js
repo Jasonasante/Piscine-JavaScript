057680b287f21d484fedc0bf8b5612388293a46c
jasonasante <jason.asante@outlook.com>
2022-06-16 16:41:38 +0100

    every(),some() method manipulation

--- long-words.js
+++ long-words.js
@@ -1,5 +1,5 @@
 const longWords=array=>array.every(str=>str.length>=5)
 
-const oneLongWords=array=>array.some(str=>str.length>=10)
+const oneLongWord=array=>array.some(str=>str.length>=10)
 
 const noLongWords=array=>array.every(str=>str.length<7)
