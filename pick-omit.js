1452bee07f37cb2ca1f5829e135ab124c497ad9a
jasonasante <jason.asante@outlook.com>
2022-06-21 00:06:26 +0100

    pick or omits key-value pairs from the recieve object with the receive string/array argument

--- pick-omit.js
+++ pick-omit.js
@@ -17,18 +17,19 @@ function pick(obj,strArr){
 }
 
 function omit(obj,strArr){
-    Object.keys(obj).forEach(key=>{
-      if (typeof strArr==='string'){
-        if (key===strArr){
-        delete obj[strArr]
-        }
-      }else{
-          for (let i=0;i<strArr.length;i++){
-          if (key===strArr[i]){
-            delete obj[strArr[i]]
+    let obj1=obj
+      Object.keys(obj).forEach(key=>{
+        if (typeof strArr==='string'){
+          if (key===strArr){
+          delete obj1[strArr]
+          }
+        }else{
+            for (let i=0;i<strArr.length;i++){
+            if (key===strArr[i]){
+              delete obj1[strArr[i]]
+            }
           }
         }
-      }
-    })
-    return obj
-}
+      })
+      return obj1
+  }
