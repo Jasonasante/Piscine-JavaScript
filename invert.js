9cca8b3302f85001692ec7e1c54fcb35094f483c
jasonasante <jason.asante@outlook.com>
2022-06-20 17:58:04 +0100

    invert the keys and values of an object

--- invert.js
+++ invert.js
@@ -1,6 +1,6 @@
 //const invertOrder = obj=> Object.keys(obj).reverse() //this reverses the order
 
-function objectFlip(obj) {
+function invert(obj) {
     const ret = {};
     Object.keys(obj).forEach(key => {
       ret[obj[key]] = key;
