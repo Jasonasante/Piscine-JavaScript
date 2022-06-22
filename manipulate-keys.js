1760f3fbdf99e836ac26cb121aa54de0b53f54b4
jasonasante <jason.asante@outlook.com>
2022-06-22 16:03:46 +0100

    manipulate-keys.js

---
+++ manipulate-keys.js
@@ -0,0 +1,54 @@
+const filterKeys=(nutrients, fun)=>{
+    let filteredNutrients={}
+    for (let [key ,value] of Object.entries(nutrients)){
+        if (fun(key)){
+         filteredNutrients[key]=value
+        }
+    }      
+    return filteredNutrients
+}
+console.log(filterKeys(nutrients, (key) => /protein/.test(key)))
+
+const mapKeys=(nutrients, fun)=>{
+    let mappedNutrients={}
+    Object.keys(nutrients).forEach(objCheck=>{
+        if(typeof nutrients[objCheck]==='object'){
+        mappedNutrients[objCheck]=fun(nutrients[objCheck])
+        }else{
+                mappedNutrients[fun(objCheck)]=nutrients[objCheck]
+        }
+    })
+    return mappedNutrients
+}
+
+const reduceKeys=(nutrients,fun, num)=>{
+    try{
+        if (nutrients.length<=1) throw new Error;
+    }catch(e) {
+        console.log(e.name)
+    } 
+    let acc
+    if (num!==undefined){
+        acc=num
+    }
+    
+    Object.keys(nutrients).forEach((objCheck,i)=>{
+        if(typeof nutrients[objCheck]==='object'){
+        Object.keys(nutrients[objCheck]).forEach((keys,i)=>{
+            if (i===0&&acc===undefined){
+                acc =keys
+            }  else{
+                 acc=fun(acc,keys)
+            }
+          })
+        }else{
+            if (i===0&&acc===undefined){
+                acc =objCheck
+            }  else{
+                 acc=fun(acc,objCheck)
+            }
+        }
+       
+    })
+     return acc
+}
