889fbc257ae2185eec1847459baebfcf86c97cd2
jasonasante <jason.asante@outlook.com>
2022-07-22 12:56:51 +0100

    pronoun.js

--- pronoun.js
+++ pronoun.js
@@ -1,5 +1,5 @@
 const pronoun=str=>{
-    let words=str.split(/(\s+)/)
+    let words=str.split(/\b\s+/)
     let res={}
     words.forEach((word,i)=>{
         if(confirmPronoun(word)){
@@ -7,15 +7,16 @@ const pronoun=str=>{
                 res[word]={words:[],count:0}
             }
             if (i !== words.length - 1 && !confirmPronoun(words[i + 1])){
-                res[word].word.push(words[index + 1])
+                res[word].words.push(words[i + 1])
             }
             res[word].count++
         }
     })
+    return res
 }
 
 const confirmPronoun=str=>{
-    const pronouns=['i','you','he','she', 'it',' they','we']
-    if (pronouns.indexOf(str)!==-1) return true
+    const pronouns=['i','you','he','she', 'it','they','we']
+    if (pronouns.indexOf(str.toLowerCase())!==-1) return true
     return false
 }
